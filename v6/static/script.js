bed702c4e0208d3ae09416475b6c925894ea0049
null1s <null1sdev@gmail.com>
2023-06-14 23:44:16 +0300

    v6

--- v6/static/script.js
+++ v6/static/script.js
@@ -1,26 +1,33 @@
 // -------------------------- //
 // HAMBURGERIO MENIU          //
-const hamburger = document.querySelector(".hamburger")
-const navMenu = document.querySelector(".nav-menu")
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navScrolled = document.querySelector("#hamb");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active")
     navMenu.classList.toggle("active")
+    navScrolled.classList.toggle("active")
 })
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active")
     navMenu.classList.remove("active")
+    navScrolled.classList.remove("active")
 }))
+
 
 // -------------------------- //
 // NAVBARO SPALVA         //
 const headerEl = document.querySelector(".navbar");
+const dropdownEl = document.querySelector(".nav-menu");
 window.addEventListener("scroll", () => {
     if (window.scrollY > 50) {
         headerEl.classList.add("nav-scrolled")
+        dropdownEl.classList.add("drop-scrolled")
     } else if (window.scrollY <= 50) {
         headerEl.classList.remove("nav-scrolled")
+        dropdownEl.classList.remove("drop-scrolled")
     }
 })
 
